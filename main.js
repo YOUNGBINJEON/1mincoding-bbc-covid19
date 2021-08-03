4c92164dbc058f1b9ed29a5c087ccee08e2da1d1
YOUNGBINJEON <sh44972@gmail.com>
2021-08-03 21:51:56 +0900

    [Update] 루프 횟수 수정

--- main.js
+++ main.js
@@ -3,9 +3,17 @@
   const graphicElems = document.querySelectorAll('.graphic-item');
   // 현재 활성화된(visible 클래스가 붙은 ) .graphic-item을 지정
   let currentItem = graphicElems[0];
+  let ioIndex;
+
+  const io = new IntersectionObserver((entries, observer)=> { 
+    ioIndex = entries[0].target.dataset.index*1;
+    console.log(ioIndex);
+    
+
+  });
 
   for (let i = 0; i< stepElems.length; i++) {
-
+    io.observe(stepElems[i]);
 
     // stepElems[i].setAttribute('data-index', i); 와 같은 방식
     stepElems[i].dataset.index = i;
@@ -23,11 +31,15 @@
     window.addEventListener('scroll', () => {
       let step;
       let boundingRect;
+      let temp = 0;
 
-      for (let i = 0; i < stepElems.length; i++) {
-        step = stepElems[i];
-        boundingRect = step.getBoundingClientRect();
-        //console.log(boundingRect.top);
+      //for (let i = 0; i < stepElems.length; i++) {
+        for (let i = ioIndex - 1; i < ioIndex + 2; i++) {  
+          step = stepElems[i];
+
+          if(!step) continue;
+          boundingRect = step.getBoundingClientRect();
+          //console.log(boundingRect.top);
 
         if(boundingRect.top > window.innerHeight * 0.1 && boundingRect.top < window.innerHeight * 0.8) {
           
