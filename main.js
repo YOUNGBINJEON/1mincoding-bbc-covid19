f91332741dea531fbd5ea0fb56a3a2f8da770855
YOUNGBINJEON <sh44972@gmail.com>
2021-08-04 23:10:13 +0900

    [Update] birdFlies 활성화

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 (() => {
+
+  const actions = {
+    birdFlies(key) {
+
+      if(key) {
+        document.querySelector('[data-index="2"] .bird').style.transform = 
+      `translateX(${window.innerWidth}px)`;
+
+      } else {
+        document.querySelector('[data-index="2"] .bird').style.transform = 
+      `translateX(-100%)`;
+
+      }
+
+      birdFlies2(key) {
+
+        if(key) {
+          document.querySelector('[data-index="5"] .bird').style.transform = 
+        `translateX(${-window.innerHeight * 0.7}px)`;
+  
+        } else {
+          document.querySelector('[data-index="5"] .bird').style.transform = 
+        `translateX(-100%)`;
+  
+        }
+      }
+  
+    }
+  };
+
   const stepElems = document.querySelectorAll('.step');
   const graphicElems = document.querySelectorAll('.graphic-item');
   // 현재 활성화된(visible 클래스가 붙은 ) .graphic-item을 지정
@@ -20,12 +50,18 @@
     graphicElems[i].dataset.index = i;
   }
 
-  function activate() {
+  function activate(action) {
     currentItem.classList.add('visible');
+    if(action) {
+      actions[action](true);
+    }
   }
 
-  function inactivate() {
+  function inactivate(action) {
     currentItem.classList.remove('visible');
+    if(action) {
+      actions[action](false);
+    }
   }
 
     window.addEventListener('scroll', () => {
@@ -46,13 +82,19 @@
           // 텍스트박스 위치 탐색
           //console.log(step.dataset.index);
           if (currentItem) {
-            inactivate();
+            inactivate(currentItem.dataset.action);
           }    
           currentItem = graphicElems[step.dataset.index];
-          activate();
+          activate(currentItem.dataset.action);
         }
       }
     });
+
+    window.addEventListener('load', ()=> {
+      setTimeout(() => scrollTo(0, 0), 100);
+    });
+
+    activate();
 
 })();
 
